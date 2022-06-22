6b7f10b42f24ff57aa411f61e2a2e97117f2090b
ZeroVassil <106878036+ZeroVassil@users.noreply.github.com>
2022-06-22 09:22:26 +0200

    Reworked EVERYTHING

--- windowFeatures.js
+++ windowFeatures.js
@@ -1,23 +1,14 @@
-function scrollDown(objectId){
-  var object = document.getElementById(objectId);
+function scrollToObject(objectId){
+  object = document.getElementById(objectId);
   object.scrollIntoView({
     behavior: "smooth",
   })
 }
 
-function scrollUp(yValue = 0){
-  absoluteY = window.scrollY;
-  if(yValue == 0){
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "smooth"
-    })
-  } else {
-    window.scrollTo({
-      top: absoluteY - yValue,
-      left: 0,
-      behavior: "smooth"
-    })
-  }
+function scrollUp(){
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+  })
 }
